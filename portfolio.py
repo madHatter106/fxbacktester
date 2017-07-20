0e771a1274f7e2d7ce4bc1b154aabb8ec977719b
madHatter106 <jkomd.283.2112@gmail.com>
2017-07-20 11:20:40 -0400

    first commit

---
+++ portfolio.py
@@ -0,0 +1,39 @@
+from abc import ABCMeta, abstractmethod
+
+
+class Portfolio():
+    """An abstract base class representing a portfolio of
+    positions (including both instruments and cash), determined
+    on the basis of a set of signals provided by a Strategy.
+
+    Inputs:
+    -------
+    Strategy signal
+        TimeStamp, signal {-1, 0, 1}
+        Pandas DataFrame
+
+    Output:
+    -------
+    Result
+        TimeStamp, equity curve, transaction cost trade record,
+        Pandas DataFrame
+
+    """
+
+    __metaclass__ = ABCMeta
+
+    def generate_positions(self):
+        """Provides the logic to determine how the portfolio
+        positions are allocated on the basis of forecasting
+        signals and available cash."""
+        raise NotImplementedError("Should implement generate_positions()!")
+
+    def backtest_portfolio(self):
+        """Provides the logic to generate the trading orders
+        and subsequent equity curve (i.e. growth of total equity),
+        as a sum of holdings and cash, and the bar-period returns
+        associated with this curve based on the 'positions' DataFrame.
+
+        Produces a portfolio object that can be examined by
+        other classes/functions."""
+        raise NotImplementedError("Should implement backtest_portfolio()!")
---
+++ strategy.py
@@ -0,0 +1,34 @@
+# backtest.py
+from abc import ABCMeta, abstractmethod
+
+class Strategy():
+    """Strategy is an abstract base class providing an interface for
+    all subsequent (inherited) trading strategies.
+
+    The goal of a (derived) Strategy object is to output a list of signals,
+    which has the form of a time series indexed pandas DataFrame.
+
+    In this instance only a single symbol/instrument is supported.
+
+    Inputs:
+    -------
+    Bar Data
+        Open, high, low, close, volume (ohlcv)
+        Pandas DataFrame
+
+    Output:
+    -------
+    Action signal
+        TimeStamp, signal {-1, 0, 1}
+        Pandas DataFrame
+    """
+
+
+
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def generate_signals(self):
+        """An implementation is required to return the DataFrame of symbols
+        containing the signals to go long, short or hold (1, -1 or 0)."""
+        raise NotImplementedError("Should implement generate_signals()!")
